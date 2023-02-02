a9a977166817473f67ef55e80d3767e50cb9b70f
Brandon Bhangari <b6_10_94_furlonge@hotmail.com>
2023-02-02 12:31:00 -0500

    added underline hover effect

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 const hamburger = document.querySelector(".hamburger-icon");
 const mobileMenu = document.querySelector(".mobile-menu");
 const bodyOverlay = document.querySelector(".body-overlay");
-const close = document.querySelectorAll(".close-icon");
+const closeOption = document.querySelectorAll(".close-icon");
+const hoverAnchorTag = document.querySelectorAll(".underline a");
 
 hamburger.addEventListener("click", () => {
   mobileMenu.style.display = "block";
   bodyOverlay.style.display = "block";
 });
 
-close.forEach((close) => {
+closeOption.forEach((close) => {
   close.addEventListener("click", () => {
     mobileMenu.style.display = "none";
     bodyOverlay.style.display = "none";
+  });
+});
+
+hoverAnchorTag.forEach((tag) => {
+  tag.addEventListener("mouseover", () => {
+    tag.parentNode.classList.add("underline-effect");
+  });
+});
+
+hoverAnchorTag.forEach((tag) => {
+  tag.addEventListener("mouseout", () => {
+    tag.parentNode.classList.remove("underline-effect");
   });
 });
